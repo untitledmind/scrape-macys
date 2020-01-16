495e1cf086b0e67774510fdca1de088e5a48c60e
Ethan Brady <50305557+untitledmind@users.noreply.github.com>
2020-01-16 14:38:00 -0500

    Add parsing functions for each level of the page

--- macys.py
+++ macys.py
@@ -17,34 +17,74 @@ def format_link(url):
         url = ''.join(['https://', url])
     return url
 
-def get_hrefs(url, agent):
-    """finds all href in BeautifulSoup and formats"""
+def get_category_href(url, agent):
+    """finds all category href from index page soup
+    returns set of formatted urls
+    """
     soup = call_soup(url, agent)
-    hrefs = [format_link(i.get('href')) for i in soup.find_all('a', href=re.compile('/shop'))]
+    hrefs = {format_link(i.get('href')) for i in soup.find_all('a', href=re.compile('/shop'))}
+    hrefs.remove(url)
+    return hrefs
+
+def get_product_href(url, agent):
+    """finds all product href from category page soup
+    returns set of formatted urls
+    """
+    soup = call_soup(url, agent)
+    hrefs = {format_link(i.get('href')) for i in soup.find_all('a', {'class': 'productDescLink'}, href=re.compile('/shop'))}
     return hrefs
 
 def get_product_data(url, agent):
-    """reads json from Macy's javascript asset"""
+    """reads json from product page soup"""
     soup = call_soup(url, agent)
     product = json.loads(soup.find('script', {'id': 'productMktData'}).text)
-    asset = json.loads(soup.find('script', {'type': 'text/javascript'}).text)
-    return product, asset
+    # asset = json.loads(soup.find('script', {'type': 'text/javascript'}).text)
+    return product #, asset
+
+# def json_to_sql(product):
+#     import sqlite3
+
+#     name = product['name']
+#     context = product['@context']
+#     category = product['category']
+#     productID = product['productID']
+#     image = product['image']
+#     url = product['url']
+#     product_type = product['type']
+#     brand = product['brand']
+#     offers = product['offers']
+#     description = product['description']
+#     currency = product['offers'][0]['itemOffered']['priceCurrency']
+#     price = product['offers'][0]['itemOffered']['price']
+#     sku = product['offers'][0]['itemOffered']['SKU']
+
+#     sqlite3.adapt
+
+#     """CREATE TABLE products
+
 
 def main():
     """begins function chain starting at Macy's index page"""
     url = 'https://www.macys.com/shop/sitemap-index?id=199462'
     agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15'}
-    categories = get_hrefs(url, agent)[:-3]
+    categories = get_category_href(url, agent)
+    print(len(categories))
+    print(categories)
+    
+    product_links = set()
+    for i, url in enumerate(categories):
+        product_links.update(get_product_href(url, agent))
+        print(f'clicked on category {i}')
+        break
 
-    products = set()
-    for index, item in enumerate(categories):
-        products_in_category = get_hrefs(item, agent)[:-3]
-        print(f'clicked on category {index}')
-        for item in products_in_category:
-            products.add(get_product_data(item, agent))
+    products = []
+    for i, url in enumerate(product_links):
+        products.append(get_product_data(url, agent))
+        print(f'clicked on product {i}')
 
-    for i in products:
-        print(i)
+    print(products)
+
+
 
 if __name__ == '__main__':
     main()
