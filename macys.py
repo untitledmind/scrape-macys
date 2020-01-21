8bf816ff12e526a23645936c7fb454b0ebc45172
Ethan Brady <50305557+untitledmind@users.noreply.github.com>
2020-01-20 23:23:44 -0500

    Add asynchronous http request functions

--- macys.py
+++ macys.py
@@ -1,28 +1,43 @@
 """Macy's web scraper
 Author: Ethan Brady
 last updated Jan 16 2020
-
 This script creates a database Macy's products by scraping their online website with BeautifulSoup and the requests module.
 Because the program makes http requests in a for-loop context, it's rather slow.
-
 Note: For speed reasons, the loop currently breaks at the first category;
 thus only the first category in the index is scraped for products.
-
 To speed up the script, next step is to implement multithreaded or asynchronous requests.
 """
 
 import requests
+import asyncio
+import aiofiles
+import aiohttp
+from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 import re
 import json
 
-def call_soup(url, agent):
+def call_soup(url: str, agent: str) -> str:
     """visits url and returns soup"""
     response = requests.get(url, headers=agent)
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
 
-def format_link(url):
+async def fetch_soup(url: str, session: ClientSession, **kwargs) -> str:
+    """GET request wrapper to fetch page html and convert to soup"""
+    response = await session.request(method='GET', url=url, **kwargs)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
+
+async def make_requests(urls: set, **kwargs):
+    async with ClientSession() as session:
+        hrefs = {
+            format_link(i)
+            for i in fetch_soup(url).find_all('a', href=re.compile('/shop'))
+            for url in urls
+        }
+
+def format_link(url: str):
     """ensures returned url starts with https://www.macys.com"""
     url = re.sub(r'^/shop/', 'https://www.macys.com/shop/', url)
     url = re.sub(r'^//www.', 'https://www.', url)
@@ -30,31 +45,37 @@ def format_link(url):
         url = ''.join(['https://', url])
     return url
 
-def get_category_href(url, agent):
+def get_category_href(url: str, agent: str) -> set:
     """finds all category href from index page
     returns set of formatted urls with index page removed
     """
     soup = call_soup(url, agent)
-    hrefs = {format_link(i.get('href')) for i in soup.find_all('a', href=re.compile('/shop'))}
+    hrefs = {
+        format_link(i.get('href'))
+        for i in soup.find_all('a', href=re.compile('/shop'))
+    }
     hrefs.remove(url)
     return hrefs
 
-def get_product_href(url, agent):
+def get_product_href(url: str, agent: str) -> set:
     """finds all product href from category page
     returns set of formatted urls
     """
     soup = call_soup(url, agent)
-    hrefs = {format_link(i.get('href')) for i in soup.find_all('a', {'class': 'productDescLink'}, href=re.compile('/shop'))}
+    hrefs = {
+        format_link(i.get('href'))
+        for i in soup.find_all('a', {'class': 'productDescLink'}, href=re.compile('/shop'))
+    }
     return hrefs
 
-def get_product_data(url, agent):
+def get_product_data(url: str, agent: str) -> dict:
     """reads json from product page soup"""
     soup = call_soup(url, agent)
     product = json.loads(soup.find('script', {'id': 'productMktData'}).text)
     # asset = json.loads(soup.find('script', {'type': 'text/javascript'}).text)
     return product #, asset
 
-def push_to_sql(products):
+def push_to_sql(products: list) -> None:
     """takes list of each product's data
     pushes to SQL table
     """
@@ -97,8 +118,10 @@ def push_to_sql(products):
             availability = product.get('offers')[0].get('availability')
             price_valid_until = product.get('offers')[0].get('priceValidUntil')
 
-            data = (product_id, name, category, image, url, product_type, brand, \
-                description, currency, price, sku, availability, price_valid_until)
+            data = (
+                product_id, name, category, image, url, product_type, brand,
+                description, currency, price, sku, availability, price_valid_until
+            )
             placeholders = ', '.join(['?'] * len(data))
             print(data)
 
@@ -107,7 +130,7 @@ def push_to_sql(products):
         c.executemany(f'INSERT INTO products VALUES ({placeholders});', rows)
         print('Pushed data to SQL table')
 
-def main():
+def main() -> None:
     """begins function chain starting at Macy's index page
     in this order:
     1. index
@@ -115,7 +138,6 @@ def main():
     3. product links
     4. product data
     5. to SQL
-
     Note: Currently the loop breaks at the first category;
     thus only the first category in the index is scraped for products.
     """
